057f7b459b92268f3e515f86536bbbf19a1bcce2
BARONYXZ <krumalmarek@gmail.com>
2024-12-03 08:52:38 +0100

    wix setup + banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,83 @@
+import Image from "next/image";
+import banner from "@/assets/banner.jpg";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { delay } from "@/lib/utils";
+import { Suspense } from "react";
+import { getWixClient } from "@/lib/wix-client.base";
+import Product from "@/components/Product";
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function page() {
   return (
-    <div>
-      <h1 className="text-red-500">STORE</h1>
+    <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
+      <div className="flex items-center bg-secondary md:h-96">
+        <div className="space-y-7 p-10 text-center md:w-1/2">
+          <h1 className="text-3xl font-bold md:text-4xl">
+            „Styl, který vyzařuje tajemství.“
+          </h1>
+          <p>
+            „Objevte unikátní umění, které oživí váš prostor. Koupíte u nás
+            kousky, které vás budou inspirovat každý den!“
+          </p>
+          <Button asChild>
+            <Link href="/shop">
+              Shop Now <ArrowRight className="ml-2 size-5" />
+            </Link>
+          </Button>
+        </div>
+        <div className="relative hidden h-full w-1/2 md:block">
+          <Image src={banner} alt="banner" className="h-full object-cover" />
+          <div className="absolute inset-0 bg-gradient-to-r from-secondary via-transparent to-transparent"></div>
+        </div>
+      </div>
+      <Suspense fallback={<LoadingSkeleton />}>
+        <FeaturedProducts />
+      </Suspense>
+    </main>
+  );
+}
+
+async function FeaturedProducts() {
+  await delay(1000);
+
+  const wixClient = getWixClient();
+
+  const { collection } =
+    await wixClient.collections.getCollectionBySlug("featured-products");
+
+  if (!collection?._id) {
+    return null;
+  }
+
+  const featuredProducts = await wixClient.products
+    .queryProducts()
+    .hasSome("collectionIds", [collection._id])
+    .descending("lastUpdated")
+    .find();
+
+  if (!featuredProducts.items.length) {
+    return null;
+  }
+
+  return (
+    <div className="space-y-5">
+      <h2 className="text-2xl font-bold">featured products</h2>
+      <div className="grid-cols2 flex gap-5 sm:grid md:grid-cols-3 lg:grid-cols-4">
+        {featuredProducts.items.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+function LoadingSkeleton() {
+  return (
+    <div className="grid-cols2 flex gap-5 p-12 sm:grid md:grid-cols-3 lg:grid-cols-4">
+      {Array.from({ length: 8 }).map((_, i) => (
+        <Skeleton key={i} className="h-[26rem] w-full" />
+      ))}
     </div>
   );
 }
