45b6afb9c1db54e72677e01e00fb7307e1f47e3c
BARONYXZ <krumalmarek@gmail.com>
2024-12-09 00:43:02 +0100

    session middleware access/refresh tokens WIX server/browser clients

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,6 +1,7 @@
 import { products } from "@wix/stores";
 import { Button, ButtonProps } from "./ui/button";
 import { addToCart } from "@/wix-api/cart";
+import { wixBrowserClient } from "@/lib/wix-client.browser";
 
 interface AddToCartButtonProps extends ButtonProps {
   product: products.Product;
@@ -18,13 +19,15 @@ export default function AddToCartButton({
   return (
     <Button
       onClick={() =>
-        addToCart({
+        addToCart(wixBrowserClient, {
           product,
           selectedOptions,
           quantity,
         })
       }
       {...props}
-    ></Button>
+    >
+      Add to cart
+    </Button>
   );
 }
--- src/wix-api/products.ts
+++ src/wix-api/products.ts
@@ -1,4 +1,5 @@
-import { getWixClient } from "@/lib/wix-client.base";
+import { getWixClient, WixClient } from "@/lib/wix-client.base";
+
 import { cache } from "react";
 
 type ProductsSort = "last_updated" | "price_asc" | "price_desc";
@@ -8,12 +9,10 @@ interface QueryProductsFilter {
   sort?: ProductsSort;
 }
 
-export async function queryProducts({
-  collectionIds,
-  sort = "last_updated",
-}: QueryProductsFilter) {
-  const wixClient = getWixClient();
-
+export async function queryProducts(
+  wixClient: WixClient,
+  { collectionIds, sort = "last_updated" }: QueryProductsFilter,
+) {
   let query = wixClient.products.queryProducts();
 
   const collectionIdsArray = collectionIds
@@ -41,22 +40,22 @@ export async function queryProducts({
   return query.find();
 }
 
-export const getProductBySlug = cache(async (slug: string) => {
-  console.log("getProductBySlug");
+export const getProductBySlug = cache(
+  async (wixClient: WixClient, slug: string) => {
+    console.log("getProductBySlug");
 
-  const wixClient = getWixClient();
+    const { items } = await wixClient.products
+      .queryProducts()
+      .eq("slug", slug)
+      .limit(1)
+      .find();
 
-  const { items } = await wixClient.products
-    .queryProducts()
-    .eq("slug", slug)
-    .limit(1)
-    .find();
+    const product = items[0];
 
-  const product = items[0];
+    if (!product || !product.visible) {
+      return null;
+    }
 
-  if (!product || !product.visible) {
-    return null;
-  }
-
-  return product;
-});
+    return product;
+  },
+);
