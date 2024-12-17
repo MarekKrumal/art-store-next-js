024f1f141feaf3d522ff62a3ec0dc60c8e4e93f8
BARONYXZ <krumalmarek@gmail.com>
2024-12-17 17:23:47 +0100

    CollectionPage(nav menu, advanced layout.tsx fetching)

--- src/wix-api/collections.ts
+++ src/wix-api/collections.ts
@@ -1,7 +1,24 @@
 import { WixClient } from "@/lib/wix-client.base";
+import { collections } from "@wix/stores";
+import { cache } from "react";
 
-export async function getCollectionBySlug(wixClient: WixClient, slug: string) {
-  const { collection } = await wixClient.collections.getCollectionBySlug(slug);
+export const getCollectionBySlug = cache(
+  async (wixClient: WixClient, slug: string) => {
+    const { collection } =
+      await wixClient.collections.getCollectionBySlug(slug);
 
-  return collection || null;
-}
+    return collection || null;
+  },
+);
+
+export const getCollections = cache(
+  async (wixClient: WixClient): Promise<collections.Collection[]> => {
+    const collections = await wixClient.collections
+      .queryCollections()
+      .ne("_id", "00000000-000000-000000-000000000001") //all products
+      .ne("_id", "4f04de11-1ac8-6832-5cf3-f0bd9b2a6754") //featured products
+      .find();
+
+    return collections.items;
+  },
+);
