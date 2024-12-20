2ac6559b807c7b1be062947fb72e19ce2d532941
BARONYXZ <krumalmarek@gmail.com>
2024-12-20 12:48:59 +0100

    sort + filter products dle ceny

--- src/app/shop/SearchFilterLayout.tsx
+++ src/app/shop/SearchFilterLayout.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ProductsSort } from "@/wix-api/products";
 import { collections } from "@wix/stores";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useOptimistic, useTransition } from "react";
+
+import { useEffect, useOptimistic, useState, useTransition } from "react";
 
 interface SearchFilterLayoutProps {
   collections: collections.Collection[];
@@ -19,6 +30,9 @@ export default function SearchFilterLayout({
 
   const [optimisticFilters, setOptimisticFilters] = useOptimistic({
     collection: searchParams.getAll("collection"),
+    price_min: searchParams.get("price_min") || undefined,
+    price_max: searchParams.get("price_max") || undefined,
+    sort: searchParams.get("sort") || undefined,
   });
 
   const [isPending, startTransition] = useTransition();
@@ -59,9 +73,24 @@ export default function SearchFilterLayout({
             updateFilters({ collection: collectionIds })
           }
         />
+        <PriceFilter
+          minDefaultInput={optimisticFilters.price_min}
+          maxDefaultInput={optimisticFilters.price_max}
+          updatePriceRange={(priceMin, priceMax) =>
+            updateFilters({
+              price_min: priceMin,
+              price_max: priceMax,
+            })
+          }
+        />
       </aside>
       <div className="w-full max-w-7xl space-y-5">
-        <div className="flex justify-center lg:justify-end">sort filter</div>
+        <div className="flex justify-center lg:justify-end">
+          <SortFilter
+            sort={optimisticFilters.sort}
+            updateSort={(sort) => updateFilters({ sort })}
+          />
+        </div>
         {children}
       </div>
     </main>
@@ -110,6 +139,95 @@ function CollectionsFilter({
           );
         })}
       </ul>
+      {selectedCollectionIds.length > 0 && (
+        <button
+          onClick={() => updateCollectionIds([])}
+          className="text-sm text-primary hover:underline"
+        >
+          CLear
+        </button>
+      )}
     </div>
+  );
+}
+
+interface PriceFilterProps {
+  minDefaultInput: string | undefined;
+  maxDefaultInput: string | undefined;
+  updatePriceRange: (min: string | undefined, max: string | undefined) => void;
+}
+
+function PriceFilter({
+  minDefaultInput,
+  maxDefaultInput,
+  updatePriceRange,
+}: PriceFilterProps) {
+  const [minInput, setMinInput] = useState(minDefaultInput);
+  const [maxInput, setMaxInput] = useState(maxDefaultInput);
+
+  useEffect(() => {
+    setMinInput(minDefaultInput || "");
+    setMaxInput(maxDefaultInput || "");
+  }, [minDefaultInput, maxDefaultInput]);
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    updatePriceRange(minInput, maxInput);
+  }
+
+  return (
+    <div className="space-y-3">
+      <div className="font-bold">Price range</div>
+      <form className="flex items-center gap-2" onSubmit={onSubmit}>
+        <Input
+          type="number"
+          name="min"
+          placeholder="Min"
+          value={minInput}
+          onChange={(e) => setMinInput(e.target.value)}
+          className="w-20"
+        />
+        <span>-</span>
+        <Input
+          type="number"
+          name="max"
+          placeholder="Max"
+          value={maxInput}
+          onChange={(e) => setMaxInput(e.target.value)}
+          className="w-20"
+        />
+        <Button type="submit">Go</Button>
+      </form>
+      {(!!minDefaultInput || !!maxDefaultInput) && (
+        <button
+          onClick={() => updatePriceRange(undefined, undefined)}
+          className="text-sm text-primary hover:underline"
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
+}
+
+interface SortFilterProps {
+  sort: string | undefined;
+  updateSort: (value: ProductsSort) => void;
+}
+
+function SortFilter({ sort, updateSort }: SortFilterProps) {
+  return (
+    <Select value={sort || "last_updated"} onValueChange={updateSort}>
+      <SelectTrigger className="w-fit gap-2 text-start">
+        <span>
+          Sort by: <SelectValue />
+        </span>
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="last_updated">Nejnovější</SelectItem>
+        <SelectItem value="price_asc">Cena (nejnižší po nejvyšší)</SelectItem>
+        <SelectItem value="price_desc">Cena (nejvyšší po nejnižší)</SelectItem>
+      </SelectContent>
+    </Select>
   );
 }
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,6 +1,6 @@
 import { delay } from "@/lib/utils";
 import { getWixServerClient } from "@/lib/wix-client.server";
-import { queryProducts } from "@/wix-api/products";
+import { ProductsSort, queryProducts } from "@/wix-api/products";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
@@ -13,6 +13,9 @@ interface PageProps {
     q?: string;
     page?: string;
     collection?: string[];
+    price_min?: string;
+    price_max?: string;
+    sort?: string;
   };
 }
 
@@ -23,7 +26,14 @@ export function generateMetadata({ searchParams: { q } }: PageProps): Metadata {
 }
 
 export default async function Page({
-  searchParams: { q, page = "1", collection: collectionIds },
+  searchParams: {
+    q,
+    page = "1",
+    collection: collectionIds,
+    price_min,
+    price_max,
+    sort,
+  },
 }: PageProps) {
   const title = q ? `Results for "${q}"` : "Products";
 
@@ -35,6 +45,9 @@ export default async function Page({
           q={q}
           page={parseInt(page)}
           collectionIds={collectionIds}
+          priceMin={price_min ? parseInt(price_min) : undefined}
+          priceMax={price_max ? parseInt(price_max) : undefined}
+          sort={sort as ProductsSort}
         />
       </Suspense>
     </div>
@@ -45,9 +58,19 @@ interface ProductResultsProps {
   q?: string;
   page: number;
   collectionIds?: string[];
+  priceMin?: number;
+  priceMax?: number;
+  sort?: ProductsSort;
 }
 
-async function ProductResult({ q, page, collectionIds }: ProductResultsProps) {
+async function ProductResult({
+  q,
+  page,
+  collectionIds,
+  priceMin,
+  priceMax,
+  sort,
+}: ProductResultsProps) {
   await delay(3000);
 
   const pageSize = 8;
@@ -57,6 +80,9 @@ async function ProductResult({ q, page, collectionIds }: ProductResultsProps) {
     limit: pageSize,
     skip: (page - 1) * pageSize,
     collectionIds,
+    priceMin,
+    priceMax,
+    sort,
   });
 
   if (page > (products.totalPages || 1)) notFound();
